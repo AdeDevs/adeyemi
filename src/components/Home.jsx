b98a27aa2e462deb4326bc542439694df577d2a0
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-20 14:54:41 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,6 @@
 function HomePage() {
     return (
         <div className="home">
-            <div className="home-nav">
-                
-            </div>
             <h1>home</h1>
             <footer>
                 <h1>01</h1>
